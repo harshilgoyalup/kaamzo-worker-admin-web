f3faae1e08e90b151c325e117294c2bc96a4d7a2
harshil-goyal <arveharshil@gmail.com>
2026-08-25 17:57:15 +0530

    Remove hardcoded Firebase keys; use NEXT_PUBLIC_ env variables

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,30 +5,12 @@ import { getStorage } from "firebase/storage";
 import { getFunctions } from "firebase/functions";
 
 const firebaseConfig = {
-  apiKey:
-    process.env.NEXT_PUBLIC_FIREBASE_API_KEY ||
-    process.env.NEXT_PUBLIC_API_KEY ||
-    "AIzaSyBBuJUlwhGV9Y_Wdx20EEFQ89GejXy4tsI",
-  authDomain:
-    process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN ||
-    process.env.NEXT_PUBLIC_AUTH_DOMAIN ||
-    "kaamzo-platform.firebaseapp.com",
-  projectId:
-    process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID ||
-    process.env.NEXT_PUBLIC_PROJECT_ID ||
-    "kaamzo-platform",
-  storageBucket:
-    process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET ||
-    process.env.NEXT_PUBLIC_STORAGE_BUCKET ||
-    "kaamzo-platform.firebasestorage.app",
-  messagingSenderId:
-    process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID ||
-    process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID ||
-    "13778754649",
-  appId:
-    process.env.NEXT_PUBLIC_FIREBASE_APP_ID ||
-    process.env.NEXT_PUBLIC_APP_ID ||
-    "1:13778754649:web:a7bbc6a104a8b9e39d6548",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
